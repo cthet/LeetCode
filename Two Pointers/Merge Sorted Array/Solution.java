8dc5f3861166398ea8383cdbc07cd028b0a44762
cthet <clement.thet@gmail.com>
2023-08-25 00:47:34 +0200

    add merge sorted array

---
+++ Two Pointers/Merge Sorted Array/Solution.java
@@ -0,0 +1,27 @@
+// Time complexity: O(m + n)
+
+// Space complexity: O(1)
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+       int p1 = m - 1, p2 = n - 1, p = m + n - 1;
+        
+        while (p1 >= 0 && p2 >= 0) {
+            if(nums1[p1]>nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;            
+        }
+
+        // In case there are remaining elements in nums2
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
+    }
+}
