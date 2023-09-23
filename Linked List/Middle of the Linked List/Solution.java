e091439b962c8de828ddedd6467b05af14defb85
cthet <clement.thet@gmail.com>
2023-09-23 23:56:18 +0200

    add container with most water

--- Linked List/Middle of the Linked List/Solution.java
+++ Linked List/Middle of the Linked List/Solution.java
@@ -16,11 +16,11 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-                ListNode tortoise = head, hare = head;
-        while (hare != null && hare.next != null) {
-            tortoise = tortoise.next;
-            hare = hare.next.next;
-        }
-        return tortoise;
+    ListNode tortoise = head, hare = head;
+      while (hare != null && hare.next != null) {
+        tortoise = tortoise.next;
+          hare = hare.next.next;
+      }
+      return tortoise;
     }
 }
