b3eac5c03ec14eeeacede805b8aec5aa56dd3091
cthet <clement.thet@gmail.com>
2023-12-08 23:30:44 +0100

    add squares of a sorted array

---
+++ Two Pointers/Squares of a Sorted Array/Solution.java
@@ -0,0 +1,24 @@
+// Time complexity O(n)
+
+// Space complexity O(n)
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        int[] result = new int[n];
+        for(int i = n-1; i>=0; i--){
+            int square;
+            if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+                square = nums[right];
+                right--;
+            } else {
+                square = nums[left];
+                left++;
+            }
+            result[i] = square * square;
+        }
+        return result;
+    }
+}
